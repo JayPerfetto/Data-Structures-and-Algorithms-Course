16d469805b9adb5cbaf71b2c8fd023f053e44b5d
Jason Perfetto <perfett_jaso@bentley.edu>
2019-11-15 18:38:19 -0500

    levelwidth solved all tests passing

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) { }
+function levelWidth(root) { 
+    const STOPPER = 's';
+    const arr = [root, STOPPER];
+    const counters = [0];
+
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if (node === STOPPER) {
+            counters.push(0);
+            arr.push(STOPPER);
+        } else {
+            arr.push(...node.children);
+            counters[counters.length - 1]++;
+        }
+    }
+    return counters;
+}
 
 module.exports = levelWidth;
