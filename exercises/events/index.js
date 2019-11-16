ab2f2aad57bf6cbfc0b1acfebd4579cfcaa843ca
Jason Perfetto <perfett_jaso@bentley.edu>
2019-11-15 20:44:35 -0500

    events solved, all tests passed

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -3,6 +3,30 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
-class Events { }
+class Events { 
+    constructor() {
+        this.events = {};
+    }
+
+    on(eventName, callback) {
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback);
+        } else {
+            this.events[eventName] = [callback];
+        }
+    }
+
+    trigger(eventName) {
+        if (this.events[eventName]) {
+            for (let cb of this.events[eventName]) {
+                cb();
+            }
+        }
+    }
+
+    off(eventName) {
+        delete this.events[eventName];
+    }
+}
 
 module.exports = Events;
